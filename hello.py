60a23c1315bdc08e4372ff5c1f4070f333bc8d20
Wojtek Mr <wojtekmr.bit@gmail.com>
2025-10-28 19:41:53 +0100

    Dodano wypisywanie tylko imion członków zespołu

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print "Hello Piotr!"
+def main():
+    team = ["Kacper Dreze", "Kacper Janczak", "Wojciech MrÓz"]
+    names = [person.split()[0] for person in team]
+    print("Imiona członków zespołu:", ", ".join(names))
+
+if __name__ == "__main__":
+    main()
